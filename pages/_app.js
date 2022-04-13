026184e179ce7d4e52fa946d4f3686af08219121
nhan2000 <nhan2000ts@gmail.com>
2022-04-13 10:01:30 +0700

    custom UI

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Navbar from './components/Navbar'
 import { auth } from './firebase'
 import { useState, useEffect } from 'react'
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState(null)
